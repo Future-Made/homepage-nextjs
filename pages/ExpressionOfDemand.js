d3e6ed52348a9a2f5c78adfaaea663c57ed6efc7
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-07-04 02:45:08 +0200

    rm unused

--- pages/ExpressionOfDemand.js
+++
@@ -1,6 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function ExpressionOfDemand() {
-    return (<>
-    <div className="card"> not much of expressions here </div>
-    </>); }
